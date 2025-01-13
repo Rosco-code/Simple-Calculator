40320e01b7eba2ae18e0eec8d3a0f7519c53d403
Tommy Adgate <rosco.adgate@gmail.com>
2025-01-13 14:04:51 -0800

    6 basic operators all ran together based on two inputs

--- calculator-code.py
+++ calculator-code.py
@@ -5,8 +5,8 @@ b = float(input("Enter the second number: "))
 
 #Operators for calculation of 'a' with 'b'.
 print("Sum: {} + {} = {}".format(a,b,a+b))
-print()
-print()
-print()
-print()
-print()
+print("Difference: {} - {} = {}".format(a,b,a-b))
+print("Product: {} * {} = {}".format(a,b,a*b))
+print("Quotient: {} / {} = {}".format(a,b,a/b))
+print("Power: {}^{} = {}".format(a,b,a**b))
+print("Division with remainder: {} / {} = {} Remainder: {}".format(a,b,a//b,a%b))
